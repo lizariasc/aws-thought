bbcd78aedf22bd4e7b2c88f01222aff09d6b49e6
Liz A <lizethariasc14@gmail.com>
2021-10-03 13:33:46 -0500

    get routes

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -7,6 +7,55 @@ const awsConfig = {
   endpoint: "http://localhost:8000",
 
 };
+
 AWS.config.update(awsConfig);
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const table = "Thoughts";
+
+// GET method at the /api/users/ endpoint. We'll be retrieving all the users' thoughts from the Thoughts table.
+router.get('/users', (req, res) => {
+    const params = {
+      TableName: table
+    };
+    // Scan return all items in the table
+    dynamodb.scan(params, (err, data) => {
+      if (err) {
+        res.status(500).json(err); // an error occurred
+      }else {
+        res.json(data.Items)
+      }
+    });
+  })
+
+  router.get('/users/:username', (req, res) => {
+    console.log(`Querying for thought(s) from ${req.params.username}.`);
+
+    const params = {
+        TableName: table,
+        KeyConditionExpression: "#un = :user",
+        ExpressionAttributeNames: {
+          "#un": "username",
+          "#ca": "createdAt",
+          "#th": "thought"
+        },
+        ExpressionAttributeValues: {
+          ":user": req.params.username
+        },
+        ProjectionExpression: "#th, #ca",
+        ScanIndexForward: false
+      };
+      dynamodb.query(params, (err, data) => {
+        if (err) {
+          console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+          res.status(500).json(err); // an error occurred
+        } else {
+          console.log("Query succeeded.");
+          res.json(data.Items)
+        }
+      });
+  });
+
+
+  
+
+  module.exports = router;
