ffc0a38a3b70430ad55c9d341f318b7ae29aff69
Liz A <lizethariasc14@gmail.com>
2021-10-11 10:23:06 -0700

    image-upload.js

---
+++ server/routes/image-upload.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+// use the package multer to provide the middleware for handling multipart/form-data (uploading files)
+const multer = require('multer');
+const AWS = require('aws-sdk');
+const paramsConfig = require('../utils/params-config');
+
+const storage = multer.memoryStorage({
+    destination: function(req, file, callback) {
+      callback(null, '');
+    }
+  });
+
+  // image is the key!
+const upload = multer({storage}).single('image');
+
+const s3 = new AWS.S3({
+    apiVersion: '2006-03-01'
+  })
+
+  router.post('/image-upload', upload, (req, res) => {
+    const params = paramsConfig(req.file);
+    // set up S3 service call
+  });
